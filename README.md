376e5fb98eb70a1503f254d26cc8916d10dcb196
stanley <stanleyanosike@gmail.com>
2022-05-10 16:16:15 -0700

    digital currency commit.

---
+++ README.md
@@ -0,0 +1,78 @@
+# Digital currency landing page Solution
+
+This is a frontend design of a UI landing page design "Raheemat Atata", posted on Twitter two days ago. 
+
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to: 
+
+- View the optimal layout for the site on all device's screen size
+- See hover states for all interactive elements on the page
+- Click on modal and menu list
+
+
+### Screenshot
+
+![](./images/NFT%20WEBSITE.png)
+
+
+### Links
+
+- Live Site URL: [Visit The Website ](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- [Vanilla javascript](https://javascript.com) 
+
+
+### What I learned
+
+I learned That taking out time to plan every step before starting any project is very important..
+
+```html
+<h1>Some HTML code I'm proud of</h1>
+```
+```css
+.proud-of-this-css {
+  color: papayawhip;
+}
+```
+```js
+const proudOfThisFunc = () => {
+  console.log('🎉')
+}
+```
+
+## Author
+
+- Twitter - [@stanleymm2](https://www.twitter.com/stanleymm2)
+
+
+## Acknowledgments
+
+Thanks to Raheemat Atata, Her UI which i saw on twitter was the motivtion needed to begin this project.
+
+# Digital-Currency
---
+++ file.js
@@ -0,0 +1,82 @@
+
+// listen  for button click
+document.getElementById("button").addEventListener("click", displayModal);  
+
+// function to display the button
+function displayModal() {  
+   document.getElementById("modal").style.display = "block";
+   
+}  
+
+//  Get close button
+let close = document.getElementById("close-button");
+
+// Close the modal
+close.addEventListener("click", function() {
+   document.getElementById("modal").style.display = "none";
+})
+
+
+
+// Get the modal background
+var modal = document.getElementsByClassName('background')[0];
+
+// When user clicks anywhere outside it, close modal
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+
+
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+const items = document.querySelectorAll(".item");
+
+/* Toggle mobile menu */
+function toggleMenu() {
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+  } else {
+    menu.classList.add("active");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+  }
+}
+
+/* Activate Submenu */
+function toggleItem() {
+  if (this.classList.contains("submenu-active")) {
+    this.classList.remove("submenu-active");
+  } else if (menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    this.classList.add("submenu-active");
+  } else {
+    this.classList.add("submenu-active");
+  }
+}
+
+/* Close Submenu From Anywhere */
+function closeSubmenu(e) {
+  if (menu.querySelector(".submenu-active")) {
+    let isClickInside = menu
+      .querySelector(".submenu-active")
+      .contains(e.target);
+
+    if (!isClickInside && menu.querySelector(".submenu-active")) {
+      menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    }
+  }
+}
+/* Event Listeners */
+toggle.addEventListener("click", toggleMenu, false);
+for (let item of items) {
+  if (item.querySelector(".submenu")) {
+    item.addEventListener("click", toggleItem, false);
+  }
+  item.addEventListener("keypress", toggleItem, false);
+}
+document.addEventListener("click", closeSubmenu, false);
